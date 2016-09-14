9510c36acf7f118b5caf980abf35c3689fd4a52c
Clark, Peter <peter.clark@ft.com>
2016-09-14 16:27:22 +0100

    Attempting to improve the anchor urls

--- checks.go
+++ checks.go
@@ -6,6 +6,8 @@ import (
 	"github.com/fatih/color"
 	"net/url"
 	"strconv"
+	"strings"
+	"errors"
 )
 
 func RunChecker(config Conf, channel chan Anchor){
@@ -25,7 +27,13 @@ func RunChecker(config Conf, channel chan Anchor){
 
 func CheckAnchor(config Conf, anchor Anchor){
 	client := config.Http
-	resp, err := client.Get(buildUrl(config, anchor))
+
+	url, err := buildUrl(config, anchor)
+	if err != nil {
+		return
+	}
+
+	resp, err := client.Get(url)
 
 	if err != nil {
 		logrus.WithField("url", anchor.Url).WithError(err).Error("Failed to make request!")
@@ -34,15 +42,25 @@ func CheckAnchor(config Conf, anchor Anchor){
 
 	if resp.StatusCode == http.StatusOK {
 		if config.Verbose {
-			color.Green("  OK! %10s:  %s", "200", anchor.Url)
+			color.Green("  %-10s %10s  %s", "OK!", "200", anchor.Url)
 		}
 	} else {
-		color.Red("  Not OK! %10s:  %s", strconv.Itoa(resp.StatusCode), anchor.Url)
+		color.Red("  %-10s %10s  %s", "Not OK!", strconv.Itoa(resp.StatusCode), anchor.Url)
 	}
 }
 
-func buildUrl(config Conf, anchor Anchor) string {
+func buildUrl(config Conf, anchor Anchor) (string, error) {
 	uri, _ := url.Parse(config.Root)
+
+	if strings.HasPrefix(anchor.Url, "#") {
+		color.Yellow("  %-10s %10s  %-100s %-50s", "Skipped!", "", anchor.Url, "It looks like an in-page anchor.")
+		return "", errors.New("Skipping because of #")
+	}
+
+	if strings.HasPrefix(anchor.Url, "http"){
+		return anchor.Url, nil
+	}
+
 	uri.Path = anchor.Url
-	return uri.String()
+	return uri.String(), nil
 }
